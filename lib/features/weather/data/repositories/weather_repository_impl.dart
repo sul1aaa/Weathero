16dfa71d2a0b3d794b817c6ba44fa96335654baa
Sultan <asansultan25@gmail.com>
2026-05-04 23:17:12 +0500

    Remove commented-out code from WeatherRepositoryImpl

--- lib/features/weather/data/repositories/weather_repository_impl.dart
+++ lib/features/weather/data/repositories/weather_repository_impl.dart
@@ -13,3 +13,41 @@ class WeatherRepositoryImpl {
 }
 
 // wefwefef
+//asaa
+// import 'package:weathero/features/weather/data/datasources/weather_remote_data_source.dart';
+// import '../models/weather_model.dart';
+
+// class WeatherRepositoryImpl {
+//   final WeatherRemoteDataSource remote;
+
+//   WeatherRepositoryImpl(this.remote);
+
+//   Future<WeatherModel> getWeather(String city) async {
+//     final json = await remote.getForecastByCity(city);
+//     return WeatherModel.fromJson(json);
+//   }
+// }import 'package:weathero/features/weather/data/datasources/weather_remote_data_source.dart';
+// import '../models/weather_model.dart';
+
+// class WeatherRepositoryImpl {
+//   final WeatherRemoteDataSource remote;
+
+//   WeatherRepositoryImpl(this.remote);
+
+//   Future<WeatherModel> getWeather(String city) async {
+//     final json = await remote.getForecastByCity(city);
+//     return WeatherModel.fromJson(json);
+//   }
+// }import 'package:weathero/features/weather/data/datasources/weather_remote_data_source.dart';
+// import '../models/weather_model.dart';
+
+// class WeatherRepositoryImpl {
+//   final WeatherRemoteDataSource remote;
+
+//   WeatherRepositoryImpl(this.remote);
+
+//   Future<WeatherModel> getWeather(String city) async {
+//     final json = await remote.getForecastByCity(city);
+//     return WeatherModel.fromJson(json);
+//   }
+// }
